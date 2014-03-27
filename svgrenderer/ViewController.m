2f5829aad040ad52487c7b9a49b8bffd7fd7f600
Johannes Schriewer <js@planetmutlu.com>
2014-03-27 13:55:00 +0100

    Update demo

--- svgrenderer/ViewController.m
+++ svgrenderer/ViewController.m
@@ -7,23 +7,54 @@
 //
 
 #import "ViewController.h"
+#import "SVGImageView.h"
+#import "UIImage+SVG.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet SVGImageView *svgView;
 @end
+
+#define MAX_HEIGHT 3000
+#define MAX_WIDTH 1500
+
+#define MIN_HEIGHT 30
+#define MIN_WIDTH 15
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [_svgView setSvgFile:@"tiger"];
+    UITapGestureRecognizer *touch = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+    [touch setNumberOfTouchesRequired:1];
+
+    [_svgView addGestureRecognizer:touch];
+#if 0
+//    UIImage *image = [[UIImage alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"tiger" ofType:@"svg"]];
+    UIImage *image = [UIImage imageNamed:@"tiger"];
+    [_svgView setHidden:YES];
+
+    UIImageView *img = [[UIImageView alloc] initWithImage:image];
+    [self.view addSubview:img];
+#endif
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)tap:(UITapGestureRecognizer *)recognizer {
+    [UIView animateWithDuration:0.5 animations:^{
+        CGRect frame = [_svgView frame];
+        frame.size.height /= 2.0;
+        [_svgView setFrame:frame];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.5 animations:^{
+            [_svgView setFrame:[[UIScreen mainScreen] applicationFrame]];
+        }];
+    }];
 }
 
 @end
